f8c041d256ac73e10ad7ea70267751bd5738933f
dprabin <dprabin@yahoo.com>
2013-07-23 20:47:40 +0000

    added css styling for responsive design to index.html. web.js modified to listen port 8080

--- web.js
+++ web.js
@@ -1,14 +1,15 @@
 var fs = require ('fs');
 var express = require('express');
+var htmlfile = "index.html";
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var buffer = fs.readFileSync("index.html");
-  response.send(buffer.toString('utf-8'));
+  var buffer = fs.readFileSync(htmlfile).toString('utf-8');
+  response.send(buffer);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
